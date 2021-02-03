999ddc2b2490454fa7f998f6a931ed2ffce46cf2
MohammadR-M <mohammadrezamirzadejava@gmail.com>
2021-02-03 18:55:06 +0330

    Complete exe1

---
+++ NodeJS/exe1/app.js
@@ -0,0 +1,11 @@
+const http = require("http");
+
+http.createServer(function (request, response) {
+        if (request.url === "/" && request.method === "GET") {
+            response.write("Hello World");
+            response.end();
+        } else {
+            response.write("Bad Request");
+            response.end();
+        }
+    }).listen(8080);
