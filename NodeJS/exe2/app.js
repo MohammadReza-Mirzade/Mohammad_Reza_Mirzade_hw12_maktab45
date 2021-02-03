698cfdef920404e1f4aad2aefce42cccd56d2019
MohammadR-M <mohammadrezamirzadejava@gmail.com>
2021-02-03 19:20:34 +0330

    Fix exe2

--- NodeJS/exe2/app.js
+++ NodeJS/exe2/app.js
@@ -6,9 +6,12 @@ http.createServer(function (request, response) {
         response.write("Hello World");
         response.end();
     } else if (request.url === "/user" && request.method === "GET") {
-        fs.readFile("file/user.json", (err, data) => {
-            response.write(data);
-            response.end();
+        fs.readFile("file/user.json", 'utf8', (err, data) => {
+            if (err){ console.log(err.message); }
+            else {
+                response.write(data);
+                response.end();
+            }
         });
     } else {
         response.write("Bad Request");
